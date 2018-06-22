144f3d1d540ed88793e955ce9ec18d925e810b49
SiyuYan <18200289607@163.com>
2018-06-22 17:11:15 +0800

    push changes

--- cost-web-ui/karma-pact.conf.js
+++ cost-web-ui/karma-pact.conf.js
@@ -13,7 +13,6 @@ module.exports = function (config) {
     // list of files / patterns to load in the browser
     files: [
       'bower_components/pact-consumer-js-dsl/dist/pact-consumer-js-dsl.js',
-      'client.js',
       'pact-test/*.js'
     ],
 
